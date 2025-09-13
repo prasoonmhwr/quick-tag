4203ea4fa5b5d62379b20d42e640cf3798861972
Prasoon Mahawar <prasoonmahawar21@gmail.com>
2025-09-13 14:30:39 +0530

    decrypting on scan

--- src/app/qr/[shortId]/route.ts
+++ src/app/qr/[shortId]/route.ts
@@ -3,6 +3,7 @@ export const revalidate = 0
 import { NextRequest, NextResponse } from 'next/server'
 import { prisma } from '@/lib/prisma'
 import { trackQRScan } from '@/lib/analytics'
+import { decryptQRData } from '@/lib/encryption'
 
 export async function GET(
   request: NextRequest,
@@ -28,20 +29,33 @@ export async function GET(
     // Track the scan
     await trackQRScan(qrCode.id, request)
 
-    // Redirect to target URL for dynamic QR codes
     if (qrCode.targetUrl) {
-      return NextResponse.redirect(new URL(qrCode.targetUrl),{
-  headers: {
-    "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
-    "Pragma": "no-cache",
-    "Expires": "0",
-    "Surrogate-Control": "no-store",
-  },
-})
+      try {
+        const dTargetUrl = decryptQRData(qrCode.targetUrl)
+        const finalTargetURl = dTargetUrl.targetUrl ? dTargetUrl.targetUrl : qrCode.targetUrl
+        return NextResponse.redirect(new URL(finalTargetURl), {
+          headers: {
+            "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
+            "Pragma": "no-cache",
+            "Expires": "0",
+            "Surrogate-Control": "no-store",
+          },
+        })
+      } catch (error) {
+        console.error('Error decrypting target URL:', error)
+        return new NextResponse('Error processing QR code', { status: 500 })
+      }
+
+    }
+    try {
+      const dContent = decryptQRData(qrCode.content)
+      const finalContent = dContent.originalContent ? dContent.originalContent : qrCode.content
+      return NextResponse.json({ content: finalContent })
+    } catch (error) {
+      console.error('Error decrypting content:', error)
+      return new NextResponse('Error processing QR code', { status: 500 })
     }
 
-    // For static QR codes, return the content
-    return NextResponse.json({ content: qrCode.content })
   } catch (error) {
     console.error('Error processing QR scan:', error)
     return NextResponse.json({ error: 'Internal server error' }, { status: 500 })
